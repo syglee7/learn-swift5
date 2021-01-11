42b2b25c7162dc2073b73f97a5fd07faadfb80c8
zenaislee <syglee7@gmail.com>
2021-01-11 21:54:58 +0900

    write comment

---
+++ HelloSwift.playground/Contents.swift
@@ -0,0 +1,14 @@
+// swift 에서 주석 쓰는 법 single line
+/*
+ 멀티라인 주석
+ */
+
+// framework import
+import /*인라인 주석*/ UIKit
+// 지도와 관련된 프레임 워크
+import MapKit
+
+var str = "Hello, playground"
+print(str)
+// 좀 더 자세히 보고 싶을 때
+dump(str)
